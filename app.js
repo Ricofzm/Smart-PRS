6844930df56f6f3fed5467a2b527992d1561e054
RxN <120871604+Ricofzm@users.noreply.github.com>
2026-06-10 12:34:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -84,11 +84,13 @@ async function loadData() {
       if (!isNaN(cons)) {
         const delta = lastCons === null ? 0 : Math.abs(cons - lastCons);
       
-        if (delta < 0.001) return; // ignore noise kecil banget
-      
-        lastCons = cons;
-        set("consumption", cons.toFixed(2));
-        push(state.consumption, cons);
+        if (delta < 0.001) {
+          // skip update consumption saja
+        } else {
+          lastCons = cons;
+          set("consumption", cons.toFixed(2));
+          push(state.consumption, cons);
+        }
       }
     }
     if (!d || Object.keys(d).length === 0) {
